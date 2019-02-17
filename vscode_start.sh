509f08fd0c03c1bc46a24e42b0ab8bfc17b8b586
Iaroslav Shcherbatyi <iaroslav.github@gmail.com>
2019-02-17 21:33:37 +0100

    Cleanup of the structure of repo, README.md

---
+++ vscode_start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script starts the VisualCode and http server for the project.
+# If the keys are not available for http-server for a dummy SSL for
+# debugging, they are generated automatically.
+
+# Settings
+export PROJECT_PATH=~/de64.github.io
+export DUMMY_FOLDER=$PROJECT_PATH/.dummykeys
+# SSL key certificate pair
+export KEY_PATH=$DUMMY_FOLDER/key.pem
+export CERT_PATH=$DUMMY_FOLDER/cert.pem
+
+# Execution
+cd $PROJECT_PATH
+# check if dummy certificate exists
+if [ ! -f $CERT_PATH ]; then
+	mkdir -p $DUMMY_FOLDER
+	openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout $KEY_PATH -out $CERT_PATH
+fi
+git pull
+code $PROJECT_PATH
+http-server -c-1 -S -C $CERT_PATH -K $KEY_PATH -o
